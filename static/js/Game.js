30585eb757dc806fd161dbdc065429546da460c5
Alvin Lin <alvin.lin@stuypulse.com>
2015-10-10 19:29:02 -0400

    fix bug where players can jump infinitely

--- static/js/Game.js
+++ static/js/Game.js
@@ -40,7 +40,6 @@ Game.create = function(socket, parentElement, position, map) {
   var scene = new THREE.Scene();
   var renderer = new THREE.WebGLRenderer();
   var uiCanvas = document.createElement('canvas');
-  uiCanvas.setAttribute('style', 'border-radius: 50px;');
   uiCanvas.width = 800;
   uiCanvas.height = 600;
   var player = Player.create(position);
