65a47e8ad2cdb523e5c269eede4061165480e7d8
Alvin Lin <alvin.lin.dev@gmail.com>
2015-11-11 19:23:45 -0500

    update to util

--- shared/Util.js
+++ shared/Util.js
@@ -1,7 +1,6 @@
 /**
- * This is a utility class containing utility methods used on the client
- * and the server. The methods in this class are accessible by both the
- * client and server side modules.
+ * This is a utility class containing utility methods used on the server and
+ * client.
  * @author Alvin Lin (alvin.lin.dev@gmail.com)
  */
 
@@ -15,19 +14,6 @@ try {
  */
 function Util() {
   throw new Error('Util should not be instantiated!');
-};
-
-/**
- * Returns true if the two given values are almost equal to each other,
- * based on an optional epsilon value.
- * @param {number} a The first value to compare.
- * @param {number} b The second value to compare.
- * @param {?epsilon=} epsilon The threshold below which they are considered
- * equal.
- */
-Util.almostEqual = function(a, b, epsilon) {
-  epsilon = epsilon ? epsilon : Constants.DEFAULT_EPSILON;
-  return Math.abs(a - b) < epsilon;
 };
 
 /**
@@ -70,18 +56,6 @@ Util.getEuclideanDistance = function(x1, y1, x2, y2) {
 };
 
 /**
- * Returns a number in a given range scaled linearly to another given range.
- * @param {number} valueA The value to scale.
- * @param {number} minA The min of the range of valueA.
- * @param {number} maxA the max of the range of valueA.
- * @param {number} minB The min of the range to scale to.
- * @param {number} maxB The max of the range to scale to.
- */
-Util.linearScale = function(valueA, minA, maxA, minB, maxB) {
-  return minB + (maxB - minB) * ((valueA - minA) / (maxA - minA));
-};
-
-/**
  * Given a value, a minimum, and a maximum, returns true if value is
  * between the minimum and maximum, inclusive of both bounds. This
  * functio will still work if min and max are switched.
@@ -113,6 +87,45 @@ Util.bound = function(val, min, max) {
 };
 
 /**
+ * Returns true if the given point is in the game environment world.
+ * @param {number} x The x-coordinate of the given point.
+ * @param {number} y The y-coordinate of the given point.
+ * @return {boolean}
+ */
+Util.inWorld = function(x, y) {
+  return Util.inBound(x, Constants.WORLD_MIN, Constants.WORLD_MAX) &&
+      Util.inBound(y, Constants.WORLD_MIN, Constants.WORLD_MAX);
+};
+
+/**
+ * Bounds a coordinate if it is outside of the game environment world.
+ * @param {number} x The x-coordinate of the given point.
+ * @param {number} y The y-coordinate of the given point.
+ * @return {[number, number]}
+ */
+Util.boundWorld = function(x, y) {
+  return [Util.bound(x, Constants.WORLD_MIN, Constants.WORLD_MAX),
+          Util.bound(y, Constants.WORLD_MIN, Constants.WORLD_MAX)];
+};
+
+/**
+ * Returns a random point inside the game environment world.
+ * @param {number=} padding Optional argument specifying how much
+ *   padding from the edge of the world this function should apply.
+ *   Defaults to 30;
+ * @return {[number, number]}
+ */
+Util.getRandomWorldPoint = function(padding) {
+  if (!padding) {
+    padding = Constants.WORLD_PADDING;
+  }
+  return [Util.randRange(Constants.WORLD_MIN + padding,
+                         Constants.WORLD_MAX - padding),
+          Util.randRange(Constants.WORLD_MIN + padding,
+                         Constants.WORLD_MAX - padding)];
+};
+
+/**
  * Returns a random floating-point number between the given min and max
  * values, exclusive of the max value.
  * @param {number} min The minimum number to generate.
@@ -120,7 +133,9 @@ Util.bound = function(val, min, max) {
  */
 Util.randRange = function(min, max) {
   if (min >= max) {
-    return (Math.random() * (min - max)) + max;
+    var swap = min;
+    min = max;
+    max = swap;
   }
   return (Math.random() * (max - min)) + min;
 };
@@ -133,7 +148,9 @@ Util.randRange = function(min, max) {
  */
 Util.randRangeInt = function(min, max) {
   if (min >= max) {
-    return Math.floor(Math.random() * (min - max)) + max;
+    var swap = min;
+    min = max;
+    max = swap;
   }
   return Math.floor(Math.random() * (max - min)) + min;
 };
