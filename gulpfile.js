852fc622bcd5f944e3d11a513fccb113644f2095
Alvin Lin <alvin.lin.dev@gmail.com>
2016-01-27 14:35:28 -0500

    updated package.json and gulpfile with new css compiler

--- gulpfile.js
+++ gulpfile.js
@@ -3,46 +3,46 @@
  * @author Alvin Lin (alvin.lin.dev@gmail.com)
  */
 
-var gulp = require('gulp');
+var gulp = require("gulp");
 
-var autoprefixer = require('gulp-autoprefixer');
-var closureCompiler = require('gulp-closure-compiler');
-var less = require('gulp-less');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
+var autoprefixer = require("gulp-autoprefixer");
+var closureCompiler = require("gulp-closure-compiler");
+var cssnano = require("gulp-cssnano");
+var less = require("gulp-less");
+var rename = require("gulp-rename");
 
-gulp.task('default', ['js', 'less']);
+gulp.task("default", ["js", "less"]);
 
-gulp.task('js', function() {
-  return gulp.src('./static/js/*.js')
+gulp.task("js", function() {
+  return gulp.src("./static/js/*.js")
     .pipe(closureCompiler({
-      compilerPath: 'bower_components/closure-compiler/compiler.jar',
-      fileName: 'minified.js'
+      compilerPath: "bower_components/closure-compiler/compiler.jar",
+      fileName: "minified.js"
     }))
-    .pipe(gulp.dest('./static/dist'));
+    .pipe(gulp.dest("./static/dist"));
 });
 
-gulp.task('less', function() {
-  return gulp.src('./static/less/styles.less')
+gulp.task("less", function() {
+  return gulp.src("./static/less/styles.less")
     .pipe(less({ compress: true}))
     .pipe(autoprefixer())
-    .pipe(minifyCss())
+    .pipe(cssnano())
     .pipe(rename(function(path) {
-      path.basename = 'minified';
-      path.extname = '.css';
+      path.basename = "minified";
+      path.extname = ".css";
     }))
-    .pipe(gulp.dest('./static/dist'));
+    .pipe(gulp.dest("./static/dist"));
 });
 
-gulp.task('watch-js', function() {
-  gulp.watch('./static/js/*.js', ['js']);
+gulp.task("watch-js", function() {
+  gulp.watch("./static/js/*.js", ["js"]);
 });
 
-gulp.task('watch-less', function() {
-  gulp.watch('./static/less/*.less', ['less']);
+gulp.task("watch-less", function() {
+  gulp.watch("./static/less/*.less", ["less"]);
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./static/js/*.js', ['js']);
-  gulp.watch('./static/less/*.less', ['less']);
+gulp.task("watch", function() {
+  gulp.watch("./static/js/*.js", ["js"]);
+  gulp.watch("./static/less/*.less", ["less"]);
 });
